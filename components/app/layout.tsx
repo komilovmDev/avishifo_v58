8dcdebaa20f2154e0b5ecf5e12a044592de9ad95
asg0d <120216920+komilovmDev@users.noreply.github.com>
2025-07-28 01:52:14 +0500

    a

--- components/app/layout.tsx
+++ components/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AviShifo',
+  description: 'Created with AviShifo',
+  generator: 'AviShifo',
 }
 
 export default function RootLayout({
